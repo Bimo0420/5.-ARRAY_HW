5f4f04f7bc025349436f60fc794fe75ee0d9e713
Алексей Викулин <vaa@megamade.ru>
2022-07-20 15:29:21 +0300

    Сделаны обязательные задания N1 и N5

--- N1/Program.cs
+++ N1/Program.cs
@@ -10,6 +10,19 @@ namespace N1
     {
         static void Main(string[] args)
         {
+            int [] array = new int[7];
+            int s = 0;
+            int k = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+                s+=array[i];
+                k++;
+      
+            }
+            Console.WriteLine();
+            Console.WriteLine(s / k);
+            Console.ReadKey();
         }
     }
 }
---
+++ N5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace N5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размерность массива:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[,] array = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    array[i, j] = ((i % 2 == 0) && (j % 2 == 0) || (i % 2 == 1) && (j % 2 == 1)) ? 1 : 0;
+
+                    Console.Write("{0} ", array[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
